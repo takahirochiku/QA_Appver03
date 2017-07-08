58865a92a9baad6b8b48f31ad2ea22d74f7097e7
takahirochiku <tik.chiku@gmail.com>
2017-07-08 17:35:37 +0900

    07/08 17:30 submit

--- app/src/main/java/jp/techacademy/chiku/takahiro/qa_app/QuestionDetailActivity.java
+++ app/src/main/java/jp/techacademy/chiku/takahiro/qa_app/QuestionDetailActivity.java
@@ -2,7 +2,9 @@ package jp.techacademy.chiku.takahiro.qa_app;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -22,11 +24,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.HashMap;
 import java.util.Map;
 
+import static android.R.attr.data;
+
 public class QuestionDetailActivity extends AppCompatActivity {
 
     private ListView mListView;
     private Question mQuestion;
-    private Question mName;
+    private String mName;
     private Question mQuestionUid;
     private Button mFavoriteButton;
     private QuestionDetailListAdapter mAdapter;
@@ -127,7 +131,12 @@ public class QuestionDetailActivity extends AppCompatActivity {
 
                 if (mQuestionUid == null) {
 
-                    mDataBaseReference.child("users").child(String.valueOf(mName)).setValue(mQuestionUid);
+                    // Preferenceから名前を取る
+                    SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(QuestionDetailActivity.this);
+                    String name = sp.getString(Const.NameKEY, "");
+
+                    mName = name;
+                    mDataBaseReference.child(Const.FavoritesPATH).child(mName).setValue(mQuestionUid);
 
                     //DatabaseReference favoriteRef = mDataBaseReference.child(Const.FavoritesPATH).child(String.valueOf(mName)).child(String.valueOf(mQuestionUid));
                     //ConstがFavoriteに、mName階層,QuestionUidを下階層にして登録？
@@ -139,7 +148,7 @@ public class QuestionDetailActivity extends AppCompatActivity {
 
                 }else {
 
-                    mDataBaseReference.child("users").child(String.valueOf(mName)).removeValue();
+                    mDataBaseReference.child(Const.FavoritesPATH).child(mName).removeValue();
 
                     Snackbar.make(findViewById(android.R.id.content), "解除しました", Snackbar.LENGTH_LONG).show();
                 }
