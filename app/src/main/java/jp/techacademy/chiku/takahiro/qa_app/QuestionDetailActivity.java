41172428c2562e0e3cb063e2462b3febe427f781
takahirochiku <tik.chiku@gmail.com>
2017-07-08 16:25:44 +0900

    07/08 16:30 submit

--- app/src/main/java/jp/techacademy/chiku/takahiro/qa_app/QuestionDetailActivity.java
+++ app/src/main/java/jp/techacademy/chiku/takahiro/qa_app/QuestionDetailActivity.java
@@ -1,10 +1,13 @@
 package jp.techacademy.chiku.takahiro.qa_app;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -17,16 +20,21 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class QuestionDetailActivity extends AppCompatActivity {
 
     private ListView mListView;
     private Question mQuestion;
-    private int mLike;
-    private Button mLikeButton;
+    private Question mName;
+    private Question mQuestionUid;
+    private Button mFavoriteButton;
     private QuestionDetailListAdapter mAdapter;
 
     private DatabaseReference mAnswerRef;
+    private DatabaseReference mFavoritesRef;
+    FirebaseAuth mAuth; //FirebaseAuthクラスを定義
+    DatabaseReference mDataBaseReference; //データベースへの読み書きに必要なDatabaseReferenceクラスを定義
 
     private ChildEventListener mEventListener = new ChildEventListener() {
         @Override
@@ -83,6 +91,12 @@ public class QuestionDetailActivity extends AppCompatActivity {
 
         setTitle(mQuestion.getTitle());
 
+
+        mDataBaseReference = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();  // FirebaseAuthのオブジェクトを取得する
+        mFavoriteButton = (Button) findViewById(R.id.FavoriteButton); //リスナー登録する
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();  // ログイン済みのユーザーを取得する
+
         // ListViewの準備
         mListView = (ListView) findViewById(R.id.listView);
 
@@ -90,7 +104,47 @@ public class QuestionDetailActivity extends AppCompatActivity {
         mListView.setAdapter(mAdapter);
         mAdapter.notifyDataSetChanged();
 
-        mLikeButton = (Button) findViewById(R.id.likeButton);
+        if (user == null) {//ログインしていない
+            mFavoriteButton.setVisibility(View.GONE); //ボタンを消す
+        }else{
+            mFavoriteButton.setVisibility(View.VISIBLE);
+        }
+
+        mFavoriteButton = (Button) findViewById(R.id.FavoriteButton);
+
+        if (mQuestionUid == null) {
+            mFavoriteButton.setText("お気に入り登録");
+        }else{
+            mFavoriteButton.setText("お気に入り解除");
+        }
+
+        mFavoriteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                InputMethodManager im = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                im.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS); //キーボードを隠す
+
+                if (mQuestionUid == null) {
+
+                    mDataBaseReference.child("users").child(String.valueOf(mName)).setValue(mQuestionUid);
+
+                    //DatabaseReference favoriteRef = mDataBaseReference.child(Const.FavoritesPATH).child(String.valueOf(mName)).child(String.valueOf(mQuestionUid));
+                    //ConstがFavoriteに、mName階層,QuestionUidを下階層にして登録？
+
+                    //Map<String, String> data = new HashMap<String, String>();
+                    //data.put("uid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                    Snackbar.make(findViewById(android.R.id.content), "登録しました", Snackbar.LENGTH_LONG).show();
+
+                }else {
+
+                    mDataBaseReference.child("users").child(String.valueOf(mName)).removeValue();
+
+                    Snackbar.make(findViewById(android.R.id.content), "解除しました", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
